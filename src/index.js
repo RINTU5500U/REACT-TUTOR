6e2bfd0182981f379d8376b1f2a5e39df9de8cdf
RINTU5500U <crowdrintu1@gmail.com>
2022-12-02 16:02:07 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
-// import App from './App'
-import ToDoList from './ToDoList'
+import App from './App'
 
-ReactDOM.render(<ToDoList/>, document.getElementById("root"))
+ReactDOM.render(<App/>, document.getElementById("root"))
 
 
 //ReactDOM.render( /*#__PURE__*/React.createElement("h1", null, "orld!! "), document.getElementById("root"));
