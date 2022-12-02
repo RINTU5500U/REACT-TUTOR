11da932cc284a00a9f73230487880c1a77e73cc3
RINTU5500U <crowdrintu1@gmail.com>
2022-12-02 19:51:00 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,14 @@ import React, { useEffect, useState } from "react";
 
 const App = () => {
   const [num, setNum] = useState(0);
-  const [nums, setNums] = useState(0);
-
-  useEffect(() => {
-    alert("clicked");
-  }, [num]);
+  useEffect(()=>{
+    document.title = `you clicked ${num} times`
+  })
+  const add = () => { setNum(num + 1)};
 
   return (
     <>
-      <button
-        onClick={() => {
-          setNum(num + 1);
-        }}
-      >
-        Click Me {num}
-      </button>
-      <br />
-      <button
-        onClick={() => {
-          setNums(nums + 1);
-        }}
-      >
-        Click Me {nums}
-      </button>
+      <button onClick={add}>Click Me {num}</button>
     </>
   );
 };
