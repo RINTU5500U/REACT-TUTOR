eb4186fc404d31bf02bfcb861c0c9fafbd857a36
RINTU5500U <crowdrintu1@gmail.com>
2022-12-03 11:29:03 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import ComA from "./ComA";
+import { Route, Switch } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import Error from "./Error";
 
 const App = () => {
+  const Name = () => {
+    return <h1>Hello, I am a Name Page</h1>;
+  };
   return (
     <>
-      <ComA/>
+      <Switch>
+        <Route exact path="/" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route path="/contact/name" component={Name} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import './index.css'
-import ComA from './ComA'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.render(<ComA/>, document.getElementById("root"))
-
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 //ReactDOM.render( /*#__PURE__*/React.createElement("h1", null, "orld!! "), document.getElementById("root"));
-
-
